b0ae610e930e25e92b3a48d98764abf220928422
kalyani joshi <kalyani121@gmail.com>
2021-10-16 16:30:36 -0400

    updated auth.js and added .env

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,13 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const secret = process.env.TOKEN_SECRET;
+const expiration = process.env.TOKEN_EXPIRATION;
 
 module.exports = {
   // function for our authenticated routes
-  // TODO: we only need the input parameter 'req'
-  // authMiddleware: function (req, res, next) {
+  
   authMiddleware: function ( { req } ) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
@@ -18,9 +17,7 @@ module.exports = {
     }
 
     if (!token) {
-      // TODO: this should return req not throwing error
       return req;
-      // return res.status(400).json({ message: 'You have no token!' });
     }
 
     // verify token and get user data out of it
@@ -29,13 +26,7 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      // TODO: we will return req at the end of this function when encountering error in  deciphering token
-      // return res.status(400).json({ message: 'invalid token!' });
     }
-
-    // TODO: we will return req instead of send to next endpoint
-    // send to next endpoint
-    // next();
     return req;
   },
   signToken: function ({ username, email, _id }) {
